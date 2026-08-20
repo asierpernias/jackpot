277beab7d75d886a119d57f848ee8988286cccee
asierpernias <asier.pernias@gmail.com>
2026-08-20 20:35:32 +0200

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,127 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "naranja":return img`
+. . . . . . . . f . . . . . . . 
+. . . . . . . . f . 7 7 7 . . . 
+. . . . . . . . f 7 7 7 7 . . . 
+. . . . 4 4 4 f f 7 7 7 . . . . 
+. . . 4 4 4 f 4 4 4 4 4 . . . . 
+. . 4 4 4 4 f 4 4 4 4 4 4 . . . 
+. . 4 4 4 4 4 4 4 4 4 4 4 . . . 
+. . 4 4 4 4 4 4 4 4 4 4 4 . . . 
+. . 4 4 4 4 4 4 4 4 4 4 4 . . . 
+. . 4 4 4 4 4 4 4 4 4 4 4 . . . 
+. . 4 4 4 4 4 4 4 4 4 4 4 . . . 
+. . 4 4 4 4 4 4 4 4 4 4 4 . . . 
+. . . 4 4 4 4 4 4 4 4 4 . . . . 
+. . . . 4 4 4 4 4 4 4 . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image2":
+            case "cereza":return img`
+. . . . . . . . 7 7 7 . . . . . 
+. . . . . 7 7 7 7 . 7 . . . . . 
+. . . . . 7 . . . . 7 . . . . . 
+. . . . 7 . . . . . 7 . . . . . 
+. . f 7 7 f f . . 7 . . . . . . 
+. f 2 7 2 2 2 f . 7 . . . . . . 
+. f 2 2 f f f f 7 . . . . . . . 
+. f 2 f 2 2 2 2 7 f . . . . . . 
+. f 2 f 2 2 2 2 2 f . . . . . . 
+. f 2 f 2 2 2 2 2 f . . . . . . 
+. . f f 2 2 2 2 2 f . . . . . . 
+. . . f 2 2 2 2 2 f . . . . . . 
+. . . . f f f f f . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image3":
+            case "ojo":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . f f f f f f f f f . . . . 
+. . f f 8 8 8 8 8 8 8 f f . . . 
+. f f f 6 8 8 8 8 6 6 f f f . . 
+. f 8 6 6 6 6 8 8 8 8 6 8 f . . 
+. f 8 8 6 f f f f f 6 8 8 f . . 
+. f 8 8 8 f f f f f 6 8 8 f . . 
+. f 8 8 8 f f f f f 8 8 8 f . . 
+. f 8 8 8 f f f f f 8 8 8 f . . 
+. f 8 8 6 f f f f f 8 8 8 f . . 
+. f 8 8 6 8 8 8 8 8 8 8 8 f . . 
+. f f f 8 8 8 8 8 8 8 8 f f . . 
+. . f f 8 8 8 8 8 8 8 f f . . . 
+. . . f f f f f f f f f . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image4":
+            case "limon":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . f . . . . 
+. . . . . . . . . . . f . . . . 
+. . . . . . . . . . f f . . . . 
+. . . . . . . 5 5 5 f 7 7 7 . . 
+. . . . . 5 5 5 5 f 7 7 7 7 7 . 
+. . . . 5 5 5 5 f f 5 7 7 7 7 . 
+. . . 5 5 5 5 5 5 5 5 5 5 . . . 
+. . 5 5 5 5 5 5 5 5 5 5 . . . . 
+. . 5 5 5 5 5 5 5 5 5 . . . . . 
+. . 5 5 5 5 5 5 5 . . . . . . . 
+. . . 5 5 5 5 5 . . . . . . . . 
+. . . . 5 5 5 . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image5":
+            case "seta":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . f f f f f f f . . . . . 
+. . . f 2 2 2 1 2 2 2 f . . . . 
+. . . f 2 1 2 2 2 1 2 f . . . . 
+. . . f 2 2 2 2 2 2 2 f . . . . 
+. . . f 2 2 2 1 2 2 2 f . . . . 
+. . . . f f f f f f f . . . . . 
+. . . . f d d d d d f . . . . . 
+. . . . f 1 1 1 1 1 f . . . . . 
+. . . . f f d d d f f . . . . . 
+. . . . . f f f f f . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("json", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
